943c30425d0279d09138600477983c6ddb14c898
0e1 <0e1@riseup.net>
2023-09-01 16:15:38 -0300

    Update readme. Create reproducibility.sh. digital-guard/preserv#144

--- data/_pk0003.01/README.md
+++ data/_pk0003.01/README.md
@@ -1,17 +1,18 @@
 <aside>
 <table align="right" style="padding: 1em">
-<tr><td>Paquete <a target="_git" title="Enlace canónico a git para este paquete." href="http://git.digital-guard.org/preserv-CO/blob/main/data/_pk0003.01"><big><b>pk0003.01</b></big></a> de <small><a target="_osmcodes" title="Jurisdicción" href="https://osm.codes/CO">CO</a></small>
+<tr><td>Paquete <a target="_git" title="Enlace canónico a git para este paquete." href="https://git.digital-guard.org/preserv-CO/blob/main/data/_pk0003.01"><big><b>pk0003.01</b></big></a> de <small><a target="_afacodes" title="Jurisdicción" href="https://afa.codes/CO">CO</a></small>
 </td></tr>
 <tr><td>
 Donante: <a rel="external" target="_doador" href="https://www.igac.gov.co/">Instituto Geográfico Agustín Codazzi</a>
 <br/>&nbsp; <small>NIT 899.999.004-9</small> • Wikidata <a rel="external" target="_doador" title="Enlace del descriptor Wikidata del donante" href="https://www.wikidata.org/wiki/Q3152313">Q3152313</a></small><br/>
 
-Obtido via <i>site</i> em <b>1900-01-01</b> por:
+Obtido via <i>site</i> em <b>01/01/1900</b> por:
 <br/>&nbsp; Avaliação técnica: <a rel="external" target="_gitPerson" title="Usuario de Git" href="https://github.com/crebollobr">crebollobr</a>
 <br/>&nbsp; Representação institucional: <a rel="external" target="_gitPerson" title="Usuario de" href="https://github.com/ThierryAJean">ThierryAJean</a><br/>
 </td></tr>
 <tr><td>Camadas: <a title="via" href="#-via"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-via.png" alt="via" width="20"/></a> <a title="block" href="#-block"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-block.png" alt="block" width="20"/></a> <a title="nsvia" href="#-nsvia"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-nsvia.png" alt="nsvia" width="20"/></a> <a title="parcel" href="#-parcel"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-parcel.png" alt="parcel" width="20"/></a> <a title="geoaddress" href="#-geoaddress"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-geoaddress.png" alt="geoaddress" width="20"/></a> </td></tr>
-
+<tr><td>Datos publicados en <a href="https://git.digital-guard.org/preservCutGeo-CO2021/tree/main/data/_pk0003.01"></a><br/><a href="#reproducibilidad">Reproducible</a></td></tr>
+<tr><td>Visualización: <a title="block" href="https://viz.addressforall.org/CO/_pk0003.01/block"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-block.png" alt="block" width="20"/></a> <a title="via" href="https://viz.addressforall.org/CO/_pk0003.01/via"><img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-via.png" alt="via" width="20"/></a> </td></tr>
 </table>
 </aside>
 
@@ -19,11 +20,32 @@ Obtido via <i>site</i> em <b>1900-01-01</b> por:
 
 Este repositorio de metadatos describe un paquete de archivos donados al dominio público. Está siendo conservado por Digital Guard: para obtener más detalles, consulte la [documentación sobre el proceso de registro y conservación](https://wiki.addressforall.org/doc/Documentação_Digital-guard).
 
-Nota. Este documento README fue generado por software a partir de la información contenida en el archivo [`make_conf.yaml`](http://git.digital-guard.org/preserv-CO/blob/main/data/_pk0003.01/make_conf.yaml) en este paquete, e información adicional de los catálogos de [donantes](https://git.digital-guard.org/preserv-BR/blob/main/data/donor.csv) y [paquetes](https://git.digital-guard.org/preserv-BR/blob/main/data/donatedPack.csv).
+Nota. Este documento README fue generado por software a partir de la información contenida en el archivo [`make_conf.yaml`](https://git.digital-guard.org/preserv-CO/blob/main/data/_pk0003.01/make_conf.yaml) en este paquete, e información adicional de los catálogos de [donantes](https://git.digital-guard.org/preserv-BR/blob/main/data/donor.csv) y [paquetes](https://git.digital-guard.org/preserv-BR/blob/main/data/donatedPack.csv).
 
 # Capas de datos
 
 Los archivos contienen "capas de datos" temáticas. Los metadatos también describen cómo se evaluó cada capa y cómo se filtraron sus datos de forma estandarizada.
+
+## <img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-block.png" alt="block" width="20"/> block
+
+Nombre del archivo: `shp/U_MANZANA`<br/>*Descarga* e integridad: [d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip](http://dl.digital-guard.org/d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip)<br/>Descripción: Manzana<br/>Tamaño del archivo: 138808602 bytes (132.38 <abbr title="mebibyte">MiB</abbr>)<br/>Formato: shp<br/>SRID: 4686
+
+#### Otros datos relevantes
+* `codigo`
+* `barrio_cod`
+
+#### Filtrado de resultados y su publicación
+253118117 bytes (241.39 <abbr title="mebibyte">MiB</abbr>)<br/>221501 polígonos con 1498.52 <abbr title="quilômetros quadrados">km²</abbr><br/>densidad media: 0.04 polígonos/km²<br/>GeoJSONs publicados em [https://git.digital-guard.org/preservCutGeo-CO2021/tree/main/data/_pk0003.01/block](https://git.digital-guard.org/preservCutGeo-CO2021/tree/main/data/_pk0003.01/block)
+
+## <img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-via.png" alt="via" width="20"/> via
+
+Nombre del archivo: `NOMENCLATURA_VIAL`<br/>*Descarga* e integridad: [6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip](http://dl.digital-guard.org/6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip)<br/>Descripción: Nomenclatura Vial urbano e rural<br/>Tamaño del archivo: 21369297 bytes (20.38 <abbr title="mebibyte">MiB</abbr>)<br/>Formato: shp<br/>SRID: 4686
+
+#### Datos relevantes
+* `texto` (via)
+
+#### Filtrado de resultados y su publicación
+41913102 bytes (39.97 <abbr title="mebibyte">MiB</abbr>)<br/>189371 segmentos con 28765.4 <abbr title="quilômetros">km</abbr><br/>densidad media: 0.16 segmentos/km²<br/>GeoJSONs publicados em [https://git.digital-guard.org/preservCutGeo-CO2021/tree/main/data/_pk0003.01/via](https://git.digital-guard.org/preservCutGeo-CO2021/tree/main/data/_pk0003.01/via)
 
 ## <img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-geoaddress.png" alt="geoaddress" width="20"/> geoaddress
 
@@ -39,14 +61,6 @@ Nombre del archivo: `TERRENO`<br/>*Descarga* e integridad: [137dc416e70776ac57c3
 #### Otros datos relevantes
 * `codigo`
 
-## <img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-block.png" alt="block" width="20"/> block
-
-Nombre del archivo: `shp/U_MANZANA`<br/>*Descarga* e integridad: [d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip](http://dl.digital-guard.org/d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip)<br/>Descripción: Manzana<br/>Tamaño del archivo: 138808602 bytes (132.38 <abbr title="mebibyte">MiB</abbr>)<br/>Formato: shp<br/>SRID: 4686
-
-#### Otros datos relevantes
-* `codigo`
-* `barrio_cod`
-
 ## <img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-nsvia.png" alt="nsvia" width="20"/> nsvia
 
 Nombre del archivo: `shp/R_VEREDA`<br/>*Descarga* e integridad: [08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip](http://dl.digital-guard.org/08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip)<br/>Descripción: Vereda<br/>Tamaño del archivo: 192682440 bytes (183.76 <abbr title="mebibyte">MiB</abbr>)<br/>Formato: shp<br/>SRID: 4686
@@ -57,19 +71,12 @@ Nombre del archivo: `shp/R_VEREDA`<br/>*Descarga* e integridad: [08bc4f124ca0a65
 #### Otros datos relevantes
 * `codigo`
 
-## <img src="https://raw.githubusercontent.com/digital-guard/preserv/main/docs/assets/layerIcon-via.png" alt="via" width="20"/> via
-
-Nombre del archivo: `NOMENCLATURA_VIAL`<br/>*Descarga* e integridad: [6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip](http://dl.digital-guard.org/6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip)<br/>Descripción: Nomenclatura Vial urbano e rural<br/>Tamaño del archivo: 21369297 bytes (20.38 <abbr title="mebibyte">MiB</abbr>)<br/>Formato: shp<br/>SRID: 4686
-
-#### Datos relevantes
-* `texto` (via)
-
 </section>
 <section>
 
 # Reproducibilidad
 
-Consulte los detalles en [reproducibility.sh](http://git.digital-guard.org/preserv-CO/blob/main/data/_pk0003.01/reproducibility.sh).
+Consulte los detalles en [reproducibility.sh](https://git.digital-guard.org/preserv-CO/blob/main/data/_pk0003.01/reproducibility.sh).
 
 </section>
 
---
+++ data/_pk0003.01/reproducibility.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+
+# layer block:
+rm -rf /tmp/sandbox/_pk17000000301_001 || true
+mkdir -m 777 -p /tmp/sandbox
+mkdir -m 777 -p /tmp/sandbox/_pk17000000301_001
+mkdir -p /tmp/pg_io
+wget -P /var/www/dl.digital-guard.org http://dl.digital-guard.org/d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip
+sudo chown postgres:www-data /var/www/dl.digital-guard.org/d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip && sudo chmod 664 /var/www/dl.digital-guard.org/d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301401_p4_block CASCADE"
+cd /tmp/sandbox/_pk17000000301_001; 7z  x -y /var/www/dl.digital-guard.org/d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip "*shp/U_MANZANA*" ; chmod -R a+rwx . > /dev/null
+psql postgres://postgres@localhost/ingest1 -c "SELECT srid, proj4text FROM spatial_ref_sys where srid=4686"
+cd /tmp/sandbox/_pk17000000301_001; shp2pgsql -D   -s 4686 "shp/U_MANZANA.shp" pk17000000301401_p4_block | psql -q postgres://postgres@localhost/ingest1 2> /dev/null
+
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.any_load('shp2sql','/tmp/sandbox/_pk17000000301_001/shp/U_MANZANA.shp','block_none','pk17000000301401_p4_block','17000000301401','d49ab53b06be4934f160bee3a92d671346d9ad2137fbd901e99875ab2fad7621.zip',array['gid', 'codigo', 'barrio_cod', 'geom'],5,1)"
+@echo "Confira os resultados nas tabelas ingest.donated_packcomponent e ingest.feature_asis".
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301401_p4_block CASCADE"
+rm -f /tmp/sandbox/_pk17000000301_001/*shp/U_MANZANA.* || true
+mkdir -m777 -p /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/block
+rm -rf /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/block/*.geojson
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.publicating_geojsons('block','CO','/var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/block','1',9,3);"
+sudo chown -R postgres:www-data /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/block
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/block -type d -exec chmod 774 {} \;
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/block -type f -exec chmod 664 {} \;
+
+
+
+
+
+# layer geoaddress:
+rm -rf /tmp/sandbox/_pk17000000301_001 || true
+mkdir -m 777 -p /tmp/sandbox
+mkdir -m 777 -p /tmp/sandbox/_pk17000000301_001
+mkdir -p /tmp/pg_io
+wget -P /var/www/dl.digital-guard.org http://dl.digital-guard.org/2db40c6a0a4ddc0bb0f765a9195c34b258de49b179f90cd54244406e0c62df83.zip
+sudo chown postgres:www-data /var/www/dl.digital-guard.org/2db40c6a0a4ddc0bb0f765a9195c34b258de49b179f90cd54244406e0c62df83.zip && sudo chmod 664 /var/www/dl.digital-guard.org/2db40c6a0a4ddc0bb0f765a9195c34b258de49b179f90cd54244406e0c62df83.zip
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301101_p1_geoaddress CASCADE"
+cd /tmp/sandbox/_pk17000000301_001; 7z  x -y /var/www/dl.digital-guard.org/2db40c6a0a4ddc0bb0f765a9195c34b258de49b179f90cd54244406e0c62df83.zip "*NOMENCLATURA_DOMICILIARIA*" ; chmod -R a+rwx . > /dev/null
+psql postgres://postgres@localhost/ingest1 -c "SELECT srid, proj4text FROM spatial_ref_sys where srid=4686"
+cd /tmp/sandbox/_pk17000000301_001; 7z x -y "*NOMENCLATURA_DOMICILIARIA*" ; chmod -R a+rx . > /dev/null
+
+cd /tmp/sandbox/_pk17000000301_001; find /tmp/sandbox/_pk17000000301_001 -path "*NOMENCLATURA_DOMICILIARIA*.shp" -exec sh -c "psql postgres://postgres@localhost/ingest1 -c 'DROP TABLE IF EXISTS pk17000000301101_p1_geoaddress'; shp2pgsql -D   -s 4686 '{}' pk17000000301101_p1_geoaddress | psql -q postgres://postgres@localhost/ingest1 ; psql postgres://postgres@localhost/ingest1 -c \"SELECT ingest.any_load('shp2sql','$$(find /tmp/sandbox/_pk17000000301_001 -path "*NOMENCLATURA_DOMICILIARIA*.shp" | head -n 1)','geoaddress_full','pk17000000301101_p1_geoaddress','17000000301101','2db40c6a0a4ddc0bb0f765a9195c34b258de49b179f90cd54244406e0c62df83.zip',array['gid', 'texto AS hnum', 'geom'],1,1,false)\" " \;
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.any_load_assign('$$(find /tmp/sandbox/_pk17000000301_001 -path "*NOMENCLATURA_DOMICILIARIA*.shp" | head -n 1)','geoaddress_full','17000000301101' )"
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301101_p1_geoaddress CASCADE"
+rm -f /tmp/sandbox/_pk17000000301_001/*NOMENCLATURA_DOMICILIARIA.* || true
+mkdir -m777 -p /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/geoaddress
+rm -rf /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/geoaddress/*.geojson
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.publicating_geojsons('geoaddress','CO','/var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/geoaddress','1',9,3);"
+sudo chown -R postgres:www-data /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/geoaddress
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/geoaddress -type d -exec chmod 774 {} \;
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/geoaddress -type f -exec chmod 664 {} \;
+
+# layer nsvia:
+rm -rf /tmp/sandbox/_pk17000000301_001 || true
+mkdir -m 777 -p /tmp/sandbox
+mkdir -m 777 -p /tmp/sandbox/_pk17000000301_001
+mkdir -p /tmp/pg_io
+wget -P /var/www/dl.digital-guard.org http://dl.digital-guard.org/08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip
+sudo chown postgres:www-data /var/www/dl.digital-guard.org/08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip && sudo chmod 664 /var/www/dl.digital-guard.org/08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301501_p5_nsvia CASCADE"
+cd /tmp/sandbox/_pk17000000301_001; 7z  x -y /var/www/dl.digital-guard.org/08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip "*shp/R_VEREDA*" ; chmod -R a+rwx . > /dev/null
+psql postgres://postgres@localhost/ingest1 -c "SELECT srid, proj4text FROM spatial_ref_sys where srid=4686"
+cd /tmp/sandbox/_pk17000000301_001; shp2pgsql -D   -s 4686 "shp/R_VEREDA.shp" pk17000000301501_p5_nsvia | psql -q postgres://postgres@localhost/ingest1 2> /dev/null
+
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.any_load('shp2sql','/tmp/sandbox/_pk17000000301_001/shp/R_VEREDA.shp','nsvia_full','pk17000000301501_p5_nsvia','17000000301501','08bc4f124ca0a65d9eae97663eca0894d3bb4d37ead1168b767a540b68db324f.zip',array['gid', 'codigo', 'nombre AS nsvia', 'geom'],5,1)"
+@echo "Confira os resultados nas tabelas ingest.donated_packcomponent e ingest.feature_asis".
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301501_p5_nsvia CASCADE"
+rm -f /tmp/sandbox/_pk17000000301_001/*shp/R_VEREDA.* || true
+mkdir -m777 -p /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/nsvia
+rm -rf /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/nsvia/*.geojson
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.publicating_geojsons('nsvia','CO','/var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/nsvia','1',9,3);"
+sudo chown -R postgres:www-data /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/nsvia
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/nsvia -type d -exec chmod 774 {} \;
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/nsvia -type f -exec chmod 664 {} \;
+
+# layer parcel:
+rm -rf /tmp/sandbox/_pk17000000301_001 || true
+mkdir -m 777 -p /tmp/sandbox
+mkdir -m 777 -p /tmp/sandbox/_pk17000000301_001
+mkdir -p /tmp/pg_io
+wget -P /var/www/dl.digital-guard.org http://dl.digital-guard.org/137dc416e70776ac57c37a4fb0cb9bedb1468e91ed73eaa656ddee91011daed7.zip
+sudo chown postgres:www-data /var/www/dl.digital-guard.org/137dc416e70776ac57c37a4fb0cb9bedb1468e91ed73eaa656ddee91011daed7.zip && sudo chmod 664 /var/www/dl.digital-guard.org/137dc416e70776ac57c37a4fb0cb9bedb1468e91ed73eaa656ddee91011daed7.zip
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301301_p3_parcel CASCADE"
+cd /tmp/sandbox/_pk17000000301_001; 7z  x -y /var/www/dl.digital-guard.org/137dc416e70776ac57c37a4fb0cb9bedb1468e91ed73eaa656ddee91011daed7.zip "*TERRENO*" ; chmod -R a+rwx . > /dev/null
+psql postgres://postgres@localhost/ingest1 -c "SELECT srid, proj4text FROM spatial_ref_sys where srid=4686"
+cd /tmp/sandbox/_pk17000000301_001; 7z x -y "*TERRENO*" ; chmod -R a+rx . > /dev/null
+
+cd /tmp/sandbox/_pk17000000301_001; find /tmp/sandbox/_pk17000000301_001 -path "*TERRENO*.shp" -exec sh -c "psql postgres://postgres@localhost/ingest1 -c 'DROP TABLE IF EXISTS pk17000000301301_p3_parcel'; shp2pgsql -D   -s 4686 '{}' pk17000000301301_p3_parcel | psql -q postgres://postgres@localhost/ingest1 ; psql postgres://postgres@localhost/ingest1 -c \"SELECT ingest.any_load('shp2sql','$$(find /tmp/sandbox/_pk17000000301_001 -path "*TERRENO*.shp" | head -n 1)','parcel_none','pk17000000301301_p3_parcel','17000000301301','137dc416e70776ac57c37a4fb0cb9bedb1468e91ed73eaa656ddee91011daed7.zip',array['gid', 'codigo', 'geom'],5,1,false)\" " \;
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.any_load_assign('$$(find /tmp/sandbox/_pk17000000301_001 -path "*TERRENO*.shp" | head -n 1)','parcel_none','17000000301301' )"
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301301_p3_parcel CASCADE"
+rm -f /tmp/sandbox/_pk17000000301_001/*TERRENO.* || true
+mkdir -m777 -p /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/parcel
+rm -rf /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/parcel/*.geojson
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.publicating_geojsons('parcel','CO','/var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/parcel','1',9,3);"
+sudo chown -R postgres:www-data /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/parcel
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/parcel -type d -exec chmod 774 {} \;
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/parcel -type f -exec chmod 664 {} \;
+
+# layer via:
+rm -rf /tmp/sandbox/_pk17000000301_001 || true
+mkdir -m 777 -p /tmp/sandbox
+mkdir -m 777 -p /tmp/sandbox/_pk17000000301_001
+mkdir -p /tmp/pg_io
+wget -P /var/www/dl.digital-guard.org http://dl.digital-guard.org/6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip
+sudo chown postgres:www-data /var/www/dl.digital-guard.org/6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip && sudo chmod 664 /var/www/dl.digital-guard.org/6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301201_p2_via CASCADE"
+cd /tmp/sandbox/_pk17000000301_001; 7z  x -y /var/www/dl.digital-guard.org/6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip "*NOMENCLATURA_VIAL*" ; chmod -R a+rwx . > /dev/null
+psql postgres://postgres@localhost/ingest1 -c "SELECT srid, proj4text FROM spatial_ref_sys where srid=4686"
+cd /tmp/sandbox/_pk17000000301_001; 7z x -y "*NOMENCLATURA_VIAL*" ; chmod -R a+rx . > /dev/null
+
+cd /tmp/sandbox/_pk17000000301_001; find /tmp/sandbox/_pk17000000301_001 -path "*NOMENCLATURA_VIAL*.shp" -exec sh -c "psql postgres://postgres@localhost/ingest1 -c 'DROP TABLE IF EXISTS pk17000000301201_p2_via'; shp2pgsql -D   -s 4686 '{}' pk17000000301201_p2_via | psql -q postgres://postgres@localhost/ingest1 ; psql postgres://postgres@localhost/ingest1 -c \"SELECT ingest.any_load('shp2sql','$$(find /tmp/sandbox/_pk17000000301_001 -path "*NOMENCLATURA_VIAL*.shp" | head -n 1)','via_full','pk17000000301201_p2_via','17000000301201','6f35dbfe7ad230f1f6f2209f5d50901c05965d7b97a9c3dafada4a9af012c335.zip',array['gid', 'texto AS via', 'geom'],5,1,false)\" " \;
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.any_load_assign('$$(find /tmp/sandbox/_pk17000000301_001 -path "*NOMENCLATURA_VIAL*.shp" | head -n 1)','via_full','17000000301201' )"
+psql postgres://postgres@localhost/ingest1 -c "DROP  TABLE IF EXISTS pk17000000301201_p2_via CASCADE"
+rm -f /tmp/sandbox/_pk17000000301_001/*NOMENCLATURA_VIAL.* || true
+mkdir -m777 -p /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/via
+rm -rf /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/via/*.geojson
+psql postgres://postgres@localhost/ingest1 -c "SELECT ingest.publicating_geojsons('via','CO','/var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/via','1',9,3);"
+sudo chown -R postgres:www-data /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/via
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/via -type d -exec chmod 774 {} \;
+sudo find /var/gits/_dg/preservCutGeo-CO2021/data/_pk0003.01/via -type f -exec chmod 664 {} \;
+
+
+
+
